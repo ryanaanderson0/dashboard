6fde5413c478f20102a253241baf4a17857f0788
Ryan Anderson <ryanaanderson0@gmail.com>
2020-11-24 13:10:12 -0800

    first commit

---
+++ react-dashboard/src/components/Layout.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';  
+import Leftside from './Leftside';  
+import Header from './Header'  
+import Footer from './Footer'
+import Content from './Content'
+ 
+ 
+export class Layout extends Component {  
+    loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>  
+    render() {  
+        return (  
+            <div>  
+                <div id="wrapper">  
+                    <Leftside></Leftside>  
+                    <div id="content-wrapper" class="d-flex flex-column">  
+                        <div id="content">  
+                            <Header />  
+                            <Content />
+                        </div>  
+                        <Footer />  
+                    </div>  
+                </div>  
+            </div>  
+        )  
+    }  
+}  
+  
+export default Layout  
